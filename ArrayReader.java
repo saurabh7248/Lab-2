d91c58908857e3001776ebdd1a60028d6653e3cb
saurabh <saurabh@openmailbox.org>
2016-04-20 00:51:17 +0530

    Added the arraylist and repaied the file Lab2.txt according to the requirememts of delimiters.

--- ArrayReader.java
+++ ArrayReader.java
@@ -1,28 +1,44 @@
-import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ArrayReader
 {
 	public static void main (String[] args) throws IOException
 	{
 		Scanner fileScan;
-		File myFile = new File ("Lab2.txt");
+		File myFile = new File ("/home/saurabh/Programs/javahelp/Lab-2/Lab2.txt");
 		fileScan = new Scanner(myFile);
 
 		String person;
+		Dataset dataset = new Dataset();
 
 		while (fileScan.hasNext())
 		{
 			person = fileScan.nextLine();
 			System.out.println(person);
 			Scanner scan2 = new Scanner(person);
-			scan2.useDelimiter("\\t");
+			scan2.useDelimiter("\t");
 
-			while (scan2.hasNext())
-				System.out.println("  " +scan2.next());
-
-				System.out.println();
+			int index = 0;
+			Object[] fields = new Object[8];
+			while (scan2.hasNext()){
+				if (index == 5) {
+					fields[index] = Integer.parseInt(scan2.next());
+				}
+				else if (index == 7) {
+					fields[index] = Double.parseDouble(scan2.next());
+				}
+				else{
+					fields[index] = scan2.next();
+				}
+				index++;
+			}
+			dataset.addData((String) fields[0], (String) fields[1], (String) fields[2], (String) fields[3],
+					(String) fields[4], (String) fields[6], (int) fields[5], (double) fields[7]);
+			scan2.close();
 		}
+		fileScan.close();
+		System.out.println(dataset.list);
 	}
 }
--- Dataset.java
+++ Dataset.java
@@ -1,26 +1,33 @@
-import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 
 public class Dataset
 {
+	public Dataset(String first, String last, String address, String city, String state, String phone, int zip,
+			double donation) {
+		super();
+		this.first = first;
+		this.last = last;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.phone = phone;
+		this.zip = zip;
+		this.donation = donation;
+	}
+
+	public Dataset() {
+	}
+
 	String first, last, address, city, state, phone;
 	int zip;
 	double donation;
 
+	static ArrayList<Dataset> list = new ArrayList<Dataset>();
 
 	public void addData(String f, String l, String a, String c, String s, String p, int z, double d)
 	{
-
-		first = f;
-		last = l;
-		address = a;
-		city = c;
-		state = s;
-		phone = p;
-		zip = z;
-		donation = d;
+		list.add(new Dataset(f, l, a, c, s, p, z, d));
 	}
 
 }
